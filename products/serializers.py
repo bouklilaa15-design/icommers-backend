035570804fde20106a61d6380bf10ac33e7d2273
bouklilaa15-design <bouklilaa15@gmail.com>
2026-08-19 20:26:10 +0100

    Fix allowed hosts

--- products/serializers.py
+++ products/serializers.py
@@ -220,6 +220,7 @@ class CommendSerializerPush(serializers.ModelSerializer):
             "willya",
             "baladiya",
             "commend_orders",
+            "is_birou",
         )
 
     def validate(self, data):
@@ -301,6 +302,7 @@ class CommendSerializerPush(serializers.ModelSerializer):
             total_price = price_and_livraison(
                 commend.willya,
                 commend.baladiya,
+                commend.is_birou,
                 unit_price,
                 quantity,
             )
